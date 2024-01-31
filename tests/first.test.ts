013c72912008bbc52adf5af76c398ae4f0b94b08
Lukáš <lukas.kanka@outlook.cz>
2024-01-31 01:25:53 +0100

    y

--- tests/first.test.ts
+++ tests/first.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/tests";
+import { test, expect } from "@playwright/test";
 import { HomePage } from "../page-objects/HomePage";
 
 // test odsouhlasení cookies
