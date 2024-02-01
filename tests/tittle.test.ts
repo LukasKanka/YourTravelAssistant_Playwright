0f7c9b6e26e9b93677e962ecebb967f6ee5bca5a
Lukáš <lukas.kanka@outlook.cz>
2024-02-01 21:24:22 +0100

    title test

---
+++ tests/tittle.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../page-objects/HomePage";
+
+// test odsouhlasení cookies
+test("TestPage", async ({ page }) => {
+  const homePage = new HomePage(page);
+  await homePage.gotoHome();
+  await expect(page).toHaveTitle("Your Travel Assistant");
+});
